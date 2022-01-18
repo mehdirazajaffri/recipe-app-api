e60840279e40d20f6b5f9ecf6e3bbb80aee6febb
Mehdi Raza Jaffri <mehdirazajaffri@gmail.com>
2022-01-18 10:07:10 +0400

    Formatting ...

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,8 +1,8 @@
 
-def add(a,b):
+def add(a, b):
     return a + b
 
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x from y and return the result"""
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,16 +2,17 @@ from django.test import TestCase
 
 from app.calc import add, subtract
 
+
 class CalcTest(TestCase):
-    
+
     def test_add_number(self):
         """Two numbers are added together"""
-        self.assertEqual(add(3,8), 11)
-        self.assertEqual(add(0,0), 0)
-        self.assertEqual(add(-1,1), 0)
-        self.assertEqual(add(-1,-1), -2)
-        self.assertEqual(add(1,1), 2)
-    
+        self.assertEqual(add(3, 8), 11)
+        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+        self.assertEqual(add(1, 1), 2)
+
     def test_substract_numbers(self):
         """Values are substracted and returned"""
-        self.assertEqual(subtract(5,11), 6)
+        self.assertEqual(subtract(5, 11), 6)
